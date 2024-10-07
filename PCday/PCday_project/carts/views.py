512310995e75e4c300fd0c8d5574671fff726cce
Paizuri <unturned80@mail.ru>
2024-10-07 15:43:00 +0300

    add ajax and other fix

--- PCday/PCday_project/carts/views.py
+++ PCday/PCday_project/carts/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
-def cart_add(request, product_id):
+from carts.models import Cart
+from carts.utils import get_user_carts
+from goods.models import Products
+
+
+def cart_add(request):
+    product_id = request.POST.get('product_id')
+    product = Products.objects.get(id=product_id)
+
+    if request.user.is_authenticated:
+        carts = Cart.object.filter(user=request.user, product=product)
+
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.object.create(user=request.user, product=product, quantity=1)
+
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
+
+
+def cart_change(request, product_slug):
     ...
 
-def cart_change(request, product_id):
-    ...
 
-def cart_remove(request, product_id):
-    ...
+def cart_remove(request, cart_id):
+    cart = Cart.object.get(id=cart_id)
+    cart.delete()
+
+    return redirect(request.META['HTTP_REFERER'])
